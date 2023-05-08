312309f903398a803aa16fd1742c8600ff130acb
ale.410 <nehringalesis@gmail.com>
2023-05-07 22:40:40 -0300

    se agrega pagina noroesta, se ajusta carrusel

--- Proyecto_final/JS/script.js
+++ Proyecto_final/JS/script.js
@@ -7,11 +7,75 @@ function openNav(){
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0";
 }
+
+const noroeste = document.querySelector("#noroeste")
+const patagonia = document.querySelector("#patagonia")
 //
+if(noroeste){
+    // Array que contiene las rutas de las imágenes del carrusel
+    const IMAGENES = [
+        '../img/cuesta_del_obispo.jpg',
+        '../img/garganta_del_diablo.jpg',
+        '../img/purmamarca.jpg',
+        '../img/salinas_grandes.jpg'
+    ];
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 3000;
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    let $imagen = document.querySelector('#imagen_carrusel');
+    let intervalo;
+
+
+// Funcion que cambia la foto en la siguiente posicion
+
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+
+// Funcion que cambia la foto en la anterior posicion
+
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+
+// Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+
+// Activa el autoplay de la imagen
+
+    function playIntervalo() {
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+    }
+
+// Eventos
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+
+// Iniciar
+    playIntervalo();
+    renderizarImagen();
+}
 
 //Funciones del carrusel
 
-window.onload = function () {
+if(patagonia) {
 
     // Array que contiene las rutas de las imágenes del carrusel
     const IMAGENES = [
